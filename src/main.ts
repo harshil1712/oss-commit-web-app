ccdea7039829f934a7ecc7548201ad89ed3b743b
Yash Dave <connect@amorpheuz.dev>
2022-01-23 20:27:11 +0530

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { ViteSSG } from 'vite-ssg'
+import routes from 'virtual:generated-pages'
+import App from './App.vue'
+
+import '@unocss/reset/tailwind.css'
+import './styles/main.css'
+import 'uno.css'
+
+export const createApp = ViteSSG(
+  App,
+  { routes, base: import.meta.env.BASE_URL },
+  (ctx) => {
+    // install all modules under `modules/`
+    Object.values(import.meta.globEager('./modules/*.ts')).forEach(i => i.install?.(ctx))
+  },
+)
